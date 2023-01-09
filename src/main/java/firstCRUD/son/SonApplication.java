e139b62482dff145b5772162c1a084acd5648c2d
son <thsgur1212@naver.com>
2023-01-09 23:24:11 +0900

    first commit

---
+++ src/main/java/firstCRUD/son/SonApplication.java
@@ -0,0 +1,16 @@
+package firstCRUD.son;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/firstCRUD/son/SonApplicationTests.java
@@ -0,0 +1,13 @@
+package firstCRUD.son;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
